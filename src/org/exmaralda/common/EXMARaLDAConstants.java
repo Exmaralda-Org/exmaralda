139b7fc1c6119cf9e6cba2e06ca5d3bc5098a207
Thomas Schmidt <thomas.schmidt@ids-mannheim.de>
2015-10-21 11:58:14 +0200

    Aufräumung

--- src/org/exmaralda/common/EXMARaLDAConstants.java
+++ src/org/exmaralda/common/EXMARaLDAConstants.java
@@ -8,6 +8,6 @@ public class EXMARaLDAConstants {
     public static String SURFER_VERSION="${surfer_version}";
     public static String ORTHONORMAL_VERSION="0.9";
     
-    public static String BUILD_TIME="20151021.0909";
+    public static String BUILD_TIME="20151021.1149";
     
 }
