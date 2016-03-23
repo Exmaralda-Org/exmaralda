02f4b205ca83dbaf7bf990c4395eb0ccc47f6ea9
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-03-23 17:02:49 +0100

    calculate more stuffs

--- src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
+++ src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
@@ -9,6 +9,11 @@ package org.exmaralda.partitureditor.partiture.transcriptionActions;
 import org.exmaralda.folker.utilities.TimeStringFormatter;
 import org.exmaralda.partitureditor.partiture.*;
 import org.exmaralda.partitureditor.jexmaralda.*;
+import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
+
+import java.util.Map;
+import java.util.HashMap;
+
 
 /**
  *
@@ -30,19 +35,81 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
 
     private void calculateTime(){
          BasicTranscription bt = table.getModel().getTranscription().makeCopy();
-         bt.getBody().getCommonTimeline().completeTimes();
+         Timeline commonTimeline = bt.getBody().getCommonTimeline();
+         commonTimeline.completeTimes();
          String html = "<html><head></head><body>";
+         // calculate per tier
+         html += "<h1>Tiers</h1>";
          html += "<table>";
          html+="<tr><th>Tier</th><th>Time</th></tr>";
-         for (int pos=0; pos<bt.getBody().getNumberOfTiers(); pos++){
-             Tier tier = bt.getBody().getTierAt(pos);
+         for (int i = 0; i < bt.getBody().getNumberOfTiers(); i++){
+             Tier tier = bt.getBody().getTierAt(i);
              double time = 
-                 tier.calculateEventTime(bt.getBody().getCommonTimeline());
+                 tier.calculateEventTime(commonTimeline);
              html+= "<tr><td>" + 
                  tier.getDescription(bt.getHead().getSpeakertable()) + "</td>";
              html+= "<td>" + 
                  TimeStringFormatter.formatMiliseconds(time*1000.0, 2)  + 
                  "</td></tr>";
+         }
+         // calculate per label etc.
+         html += "<h2>Labels (per Tier)</h2>";
+         html += "<table>";
+         html += "<tr><th>Label</th><th>Time</th></tr>";
+         Map<String, Double> totalLabelTimes = new HashMap<String, Double>();
+         for (int i = 0; i < bt.getBody().getNumberOfTiers(); i++) {
+            Tier tier = bt.getBody().getTierAt(i);
+            html += "<tr><th colspan='2'>" + 
+                tier.getDescription(bt.getHead().getSpeakertable()) + 
+                "</th></tr>";
+            Map<String, Double> labelTimes = new HashMap<String, Double>();
+            for (int j = 0; j < tier.getNumberOfEvents(); j++) {
+                Event e = tier.getEventAt(j);
+                try {
+                    double timestart = commonTimeline.getTimelineItemWithID(
+                            e.getStart()).getTime();
+                    double timeend = commonTimeline.getTimelineItemWithID(
+                            e.getEnd()).getTime();
+                    double time = timeend - timestart;
+                    String descr = e.getDescription();
+                    if (labelTimes.containsKey(descr)) {
+                        double newtime = labelTimes.get(descr) + time;
+                        labelTimes.put(descr, newtime);
+                    } else {
+                        labelTimes.put(descr, time);
+                    }
+                    if (totalLabelTimes.containsKey(descr)) {
+                        double newtime = totalLabelTimes.get(descr) + time;
+                        totalLabelTimes.put(descr, newtime);
+                    } else {
+                        totalLabelTimes.put(descr, time);
+                    }
+                } catch (JexmaraldaException je) {
+                    je.printStackTrace();
+                    // we cannot do anything here
+                }
+            }
+            for (Map.Entry<String, Double> entry : labelTimes.entrySet()) {
+                if (entry.getKey().length() < 32) {
+                    html += "<tr><td>" + entry.getKey() + "</td><td>" + 
+                        entry.getValue() + "</td></tr>";
+                } else {
+                    html += "<tr><td>" + entry.getKey().substring(0, 32) + 
+                        "... (truncated)</td><td>" + entry.getValue() +
+                        "</td></tr>";
+                }
+            }
+         }
+         html += "<tr><th colspan='2'><strong>Totals</strong></th></tr>";
+         for (Map.Entry<String, Double> entry : totalLabelTimes.entrySet()) {
+            if (entry.getKey().length() < 32) {
+                html += "<tr><td>" + entry.getKey() + "</td><td>" + 
+                    entry.getValue() + "</td></tr>";
+            } else {
+                html += "<tr><td>" + entry.getKey().substring(0, 32) + 
+                    "... (truncated)</td><td>" + entry.getValue() +
+                    "</td></tr>";
+            }
          }
          html+="</table>";
          html+="</html>";
