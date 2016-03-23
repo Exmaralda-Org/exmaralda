ea2337968560635f3dbad2800d20834f3155a5bb
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-03-23 12:09:31 +0100

    reformat

--- src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
+++ src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
@@ -13,9 +13,10 @@ import org.exmaralda.partitureditor.jexmaralda.*;
 /**
  *
  * @author  thomas
+ * @author  tpirinen
  */
 public class CalculateTimeAction extends AbstractFSMSegmentationAction {
-    
+
     /** Creates a new instance of CountDIDAAction */
     public CalculateTimeAction(PartitureTableWithActions t) {
         super("Calculate annotated time...", t);
@@ -26,24 +27,33 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
         table.commitEdit(true);
         calculateTime();
     }
-    
+
     private void calculateTime(){
          BasicTranscription bt = table.getModel().getTranscription().makeCopy();
          bt.getBody().getCommonTimeline().completeTimes();
-         String html = "<html><head></head><body><table>";
+         String html = "<html><head></head><body>";
+         html += "<table>";
          html+="<tr><th>Tier</th><th>Time</th></tr>";
          for (int pos=0; pos<bt.getBody().getNumberOfTiers(); pos++){
              Tier tier = bt.getBody().getTierAt(pos);
-             double time = tier.calculateEventTime(bt.getBody().getCommonTimeline());
-             html+= "<tr><td>" + tier.getDescription(bt.getHead().getSpeakertable()) + "</td>";
-             html+= "<td>" + TimeStringFormatter.formatMiliseconds(time*1000.0, 2)  + "</td></tr>";
+             double time = 
+                 tier.calculateEventTime(bt.getBody().getCommonTimeline());
+             html+= "<tr><td>" + 
+                 tier.getDescription(bt.getHead().getSpeakertable()) + "</td>";
+             html+= "<td>" + 
+                 TimeStringFormatter.formatMiliseconds(time*1000.0, 2)  + 
+                 "</td></tr>";
          }
-         html+="</table></html>";
+         html+="</table>";
+         html+="</html>";
          org.exmaralda.partitureditor.exSync.swing.MessageDialog md =
-                 new org.exmaralda.partitureditor.exSync.swing.MessageDialog((javax.swing.JFrame)table.parent, true, new StringBuffer(html));
-         md.setTitle(org.exmaralda.common.helpers.Internationalizer.getString("Annotated time"));
+                 new org.exmaralda.partitureditor.exSync.swing.MessageDialog(
+                         (javax.swing.JFrame)table.parent, true,
+                         new StringBuffer(html));
+         md.setTitle(org.exmaralda.common.helpers.Internationalizer.getString(
+                     "Annotated time"));
          md.show();
 
     }
-    
+
 }
