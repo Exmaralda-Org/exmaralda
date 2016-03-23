cd8647f6db9d8cfb5713fc002e99ab937a64f210
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-03-23 18:34:00 +0100

    pretty printed HTML and times and close one table

--- src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
+++ src/org/exmaralda/partitureditor/partiture/transcriptionActions/CalculateTimeAction.java
@@ -37,11 +37,11 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
          BasicTranscription bt = table.getModel().getTranscription().makeCopy();
          Timeline commonTimeline = bt.getBody().getCommonTimeline();
          commonTimeline.completeTimes();
-         String html = "<html><head></head><body>";
+         String html = "<html><head></head><body>\n";
          // calculate per tier
-         html += "<h1>Tiers</h1>";
-         html += "<table>";
-         html+="<tr><th>Tier</th><th>Time</th></tr>";
+         html += "<h1>Tiers</h1>\n";
+         html += "<table>\n";
+         html+="<tr><th>Tier</th><th>Time</th></tr>\n";
          for (int i = 0; i < bt.getBody().getNumberOfTiers(); i++){
              Tier tier = bt.getBody().getTierAt(i);
              double time = 
@@ -50,18 +50,19 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
                  tier.getDescription(bt.getHead().getSpeakertable()) + "</td>";
              html+= "<td>" + 
                  TimeStringFormatter.formatMiliseconds(time*1000.0, 2)  + 
-                 "</td></tr>";
+                 "</td></tr>\n";
          }
          // calculate per label etc.
-         html += "<h2>Labels (per Tier)</h2>";
-         html += "<table>";
-         html += "<tr><th>Label</th><th>Time</th></tr>";
+         html += "</table>\n";
+         html += "<h2>Labels (per Tier)</h2>\n";
+         html += "<table>\n";
+         html += "<tr><th>Label</th><th>Time</th></tr>\n";
          Map<String, Double> totalLabelTimes = new HashMap<String, Double>();
          for (int i = 0; i < bt.getBody().getNumberOfTiers(); i++) {
             Tier tier = bt.getBody().getTierAt(i);
             html += "<tr><th colspan='2'>" + 
                 tier.getDescription(bt.getHead().getSpeakertable()) + 
-                "</th></tr>";
+                "</th></tr>\n";
             Map<String, Double> labelTimes = new HashMap<String, Double>();
             for (int j = 0; j < tier.getNumberOfEvents(); j++) {
                 Event e = tier.getEventAt(j);
@@ -71,7 +72,7 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
                     double timeend = commonTimeline.getTimelineItemWithID(
                             e.getEnd()).getTime();
                     double time = timeend - timestart;
-                    String descr = e.getDescription();
+                    String descr = e.getDescription().trim();
                     if (labelTimes.containsKey(descr)) {
                         double newtime = labelTimes.get(descr) + time;
                         labelTimes.put(descr, newtime);
@@ -91,28 +92,35 @@ public class CalculateTimeAction extends AbstractFSMSegmentationAction {
             }
             for (Map.Entry<String, Double> entry : labelTimes.entrySet()) {
                 if (entry.getKey().length() < 32) {
-                    html += "<tr><td>" + entry.getKey() + "</td><td>" + 
-                        entry.getValue() + "</td></tr>";
+                    html += "<tr><td>" + entry.getKey() + "</td>\n<td>" + 
+                        TimeStringFormatter.formatMiliseconds(
+                                entry.getValue() * 1000.0, 2) + "</td></tr>\n";
                 } else {
                     html += "<tr><td>" + entry.getKey().substring(0, 32) + 
-                        "... (truncated)</td><td>" + entry.getValue() +
-                        "</td></tr>";
+                        "<span style='color:red'>... [[truncated]]</span>" +
+                        "</td>\n<td>" + TimeStringFormatter.formatMiliseconds(
+                                entry.getValue() * 1000.0, 2) +
+                        "</td></tr>\n";
                 }
             }
          }
-         html += "<tr><th colspan='2'><strong>Totals</strong></th></tr>";
+         html += "<tr><th colspan='2'><strong>Totals</strong></th></tr>\n";
          for (Map.Entry<String, Double> entry : totalLabelTimes.entrySet()) {
             if (entry.getKey().length() < 32) {
-                html += "<tr><td>" + entry.getKey() + "</td><td>" + 
-                    entry.getValue() + "</td></tr>";
+                html += "<tr><td>" + entry.getKey() + "</td>\n<td>" + 
+                    TimeStringFormatter.formatMiliseconds(
+                            entry.getValue() * 1000.0, 2) + "</td></tr>\n";
             } else {
                 html += "<tr><td>" + entry.getKey().substring(0, 32) + 
-                    "... (truncated)</td><td>" + entry.getValue() +
-                    "</td></tr>";
+                    "<span style='color: red'>... [[truncated]]</span>" +
+                    "</td>\n<td>" + 
+                    TimeStringFormatter.formatMiliseconds(
+                            entry.getValue() * 1000.0, 2) +
+                    "</td></tr>\n";
             }
          }
-         html+="</table>";
-         html+="</html>";
+         html+="</table>\n";
+         html+="</html>\n";
          org.exmaralda.partitureditor.exSync.swing.MessageDialog md =
                  new org.exmaralda.partitureditor.exSync.swing.MessageDialog(
                          (javax.swing.JFrame)table.parent, true,
