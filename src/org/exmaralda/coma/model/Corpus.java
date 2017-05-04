6b067813bb9e458cb379bca608195d0eb0d0e06d
Kai Wörner <herrner@gmail.com>
2017-05-04 14:59:29 +0200

    removed unused code

--- src/org/exmaralda/coma/model/Corpus.java
+++
@@ -1,9 +0,0 @@
-package org.exmaralda.coma.model;
-
-import java.util.HashMap;
-
-public class Corpus {
-    HashMap <String,Communication> communications;
-    HashMap <String,Connection> connections;
-    HashMap <String,Speaker> speakers;
-}
