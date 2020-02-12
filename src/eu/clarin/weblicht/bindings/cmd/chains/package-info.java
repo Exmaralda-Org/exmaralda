49f3115ec82750d20f4f1dc76b23a55addb15205
berndmoos <bernd.moos@googlemail.com>
2020-02-12 09:47:34 +0100

    Deleted weblicht packages (transferred to weblicht.jar) and sbcsae package (not needed anymore), issue #203

--- src/eu/clarin/weblicht/bindings/cmd/chains/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.clarin.eu/cmd/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-xmlns = {@javax.xml.bind.annotation.XmlNs(prefix = "cmd", namespaceURI = "http://www.clarin.eu/cmd/")})
-package eu.clarin.weblicht.bindings.cmd.chains;
