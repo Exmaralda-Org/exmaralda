7445b69fb38c82c751376135f50cca9802f1abd4
berndmoos <bernd.moos@googlemail.com>
2021-11-25 23:01:58 +0100

    #295 : Shdoomle

---
+++ src/org/exmaralda/partitureditor/jexmaralda/convert/FrazierADCConverter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.exmaralda.partitureditor.jexmaralda.convert;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.exmaralda.partitureditor.jexmaralda.BasicTranscription;
+
+/**
+ *
+ * @author thomas.schmidt
+ */
+public class FrazierADCConverter {
+    
+    // issue #296
+    
+    public BasicTranscription readFrazierADCFromFile(File inFile){
+        // might use Jackson to do this or maybe not
+        return null;
+    }
+    
+}
