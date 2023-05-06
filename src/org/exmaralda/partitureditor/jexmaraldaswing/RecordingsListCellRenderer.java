835fc1a7379246b4f95ae45990556861ba3d6666
berndmoos <bernd.moos@googlemail.com>
2023-05-06 15:16:03 +0200

    Beautification to RecordingsListCellRenderer

--- src/org/exmaralda/partitureditor/jexmaraldaswing/RecordingsListCellRenderer.java
+++ src/org/exmaralda/partitureditor/jexmaraldaswing/RecordingsListCellRenderer.java
@@ -36,29 +36,59 @@ public class RecordingsListCellRenderer implements ListCellRenderer {
         }
         //JLabel label = new JLabel();
         JLabel mediaTypeLabel = (JLabel) dflcr.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        mediaTypeLabel.setText(file.getName());
+        //mediaTypeLabel.setText(file.getName());
         if (path.startsWith("---")){            
             mediaTypeLabel.setIcon(new javax.swing.ImageIcon(
                     getClass().getResource("/org/exmaralda/folker/tangoicons/tango-icon-theme-0.8.1/16x16/status/dialog-error.png")));
         } else if (file.getName().toUpperCase().matches("^.*\\.(MP3|WAV|OGG|WMA|AIF)$")){
             mediaTypeLabel.setIcon(new javax.swing.ImageIcon(
                     getClass().getResource("/org/exmaralda/folker/tangoicons/tango-icon-theme-0.8.1/16x16/mimetypes/audio-x-generic.png")));
-        } else {
+        } else if (file.getName().toUpperCase().matches("^.*\\.(MPG|MP4|AVI|MOV|WEBM|DIVX|WMV)$")){
             mediaTypeLabel.setIcon(new javax.swing.ImageIcon(
                     getClass().getResource("/org/exmaralda/folker/tangoicons/tango-icon-theme-0.8.1/16x16/mimetypes/video-x-generic.png")));            
+        } else {
+            mediaTypeLabel.setIcon(new javax.swing.ImageIcon(
+                    getClass().getResource("/org/exmaralda/folker/tangoicons/tango-icon-theme-0.8.1/16x16/status/image-missing.png")));            
         }
+        
         if (file.exists()){
-            mediaTypeLabel.setForeground(Color.black);
+            //mediaTypeLabel.setForeground(Color.black);
             mediaTypeLabel.setToolTipText(path);            
         } else {
-            mediaTypeLabel.setForeground(Color.red);
+            //mediaTypeLabel.setForeground(Color.red);
             mediaTypeLabel.setToolTipText(path + " does not exist.");            
         }
         // BOLD FOR RECOMMENDED FILE TYPES
         // added MP4 for issue #219
         if (file.getName().toUpperCase().matches("^.*\\.(WAV|MPG|MP4)$")){
-            mediaTypeLabel.setFont(mediaTypeLabel.getFont().deriveFont(Font.BOLD));
+            //mediaTypeLabel.setFont(mediaTypeLabel.getFont().deriveFont(Font.BOLD));
         }
+
+        String html = "<html>";
+        boolean fileExists = file.exists();
+        boolean isRecommended = file.getName().toUpperCase().matches("^.*\\.(WAV|MPG|MP4)$");
+        if (isRecommended) html+="<b>";
+        html+="<span style=\"font-size:14pt;";
+        if (fileExists) {
+            html+="color:black";
+        } else {
+            html+="color:red";
+        }
+        html+="\">";
+        html+=file.getName();
+        String color = "gray";
+        if (isSelected) color="lightGray";
+        html+="</span>";
+        if (isRecommended) html+="</b>";        
+        html+="<br/><span style=\"color:" + color + "; font-size:10pt;\">";
+        html+=file.getAbsolutePath().substring(Math.max(0, file.getAbsolutePath().length()-60));
+        html+="</span></html>";
+        
+        mediaTypeLabel.setText(html);
+        
+        
+
+
         return mediaTypeLabel;
         
     }
