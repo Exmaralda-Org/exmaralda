41a3e963ca8ebd14d3849bd37f99c0094a5822a1
berndmoos <bernd.moos@googlemail.com>
2023-06-29 22:19:01 +0200

    all wordlists in UTF-8

--- src/org/exmaralda/orthonormal/lexicon/AbstractSimpleWordlist.java
+++ src/org/exmaralda/orthonormal/lexicon/AbstractSimpleWordlist.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Iterator;
 import org.exmaralda.orthonormal.utilities.WordUtilities;
@@ -47,7 +48,7 @@ public class AbstractSimpleWordlist {
     public void init(String path) throws IOException {
         // read the Wordlist from the internal file
         InputStream is = getClass().getResourceAsStream(path);
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
         String nextLine;
         while ((nextLine = br.readLine()) != null) {
             allWords.add(nextLine);
